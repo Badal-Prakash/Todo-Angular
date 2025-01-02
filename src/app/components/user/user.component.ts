378bdb365cb7d95184b5516e0c8a07c00ba1ea6b
badal <badal@badals-MacBook-Air.local>
2025-01-02 21:16:30 +0530

    error fixed

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component ,signal,computed, input} from '@angular/core';
-// import { DUMMY_USERS } from '../../dummy-users';
-// const random_Number=Math.floor(Math.random()*DUMMY_USERS.length);
+import { Component ,signal,computed} from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-users';
+const random_Number=Math.floor(Math.random()*DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone:true,
@@ -9,20 +9,11 @@ import { Component ,signal,computed, input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // selectedUser=signal(DUMMY_USERS[random_Number]);
-  // imagePath=computed(()=>{ return '/assets/users/' + this.selectedUser().avatar})
-  // get imagePath()
-  // {
-  //     return '/assets/users/' + this.selectedUser().avatar
-  // }
-
-
-  @input() avatar; 
-
+  selectedUser=signal(DUMMY_USERS[random_Number]);
+  imagePath=computed(()=>{ return '/assets/users/' + this.selectedUser().avatar})
 
   onSelectUSer()
   {
-    // const random_Number=Math.floor(Math.random()*DUMMY_USERS.length);
-    // this.selectedUser.set(DUMMY_USERS[random_Number]);
+
   }
 }
